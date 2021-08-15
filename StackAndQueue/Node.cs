6542b5214d664b0742d87492eedf3904b68222d5
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-15 12:46:54 +0530

    feat:Create stack

---
+++ StackAndQueue/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    class Node
+    {
+        //Instances
+        public int data;
+        public Node next;
+
+        //Constructor
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Display Welcome Message
+            Console.WriteLine("Welcome to Stack and Queue !");
+            Console.WriteLine("----------------------------");
+
+            //Calling Class and Adding Data in Stack
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+
+            //Display Message
+            linkedListStack.Display();
+        }
+    }
+}
